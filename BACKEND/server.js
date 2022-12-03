17bb5d490e9c8fb8a1cd1f23697f006066022347
Ramesh Lakmal <66841859+rameshlakmal@users.noreply.github.com>
2022-12-03 23:50:02 +0530

    Mongodb Configuration

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -13,3 +13,19 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopologyL: true,
+    useFindAndModify: false
+});
+
+const connection = mongoose.connection;
+connection.once("open",()=>{
+    console.log("Mongodb Connection Success !!!");
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server is up and running on port ${PORT}`)
+})
