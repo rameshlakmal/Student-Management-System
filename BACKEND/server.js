4e5b3492a0bb48f1c816053d247c8db03068e501
Ramesh Lakmal <66841859+rameshlakmal@users.noreply.github.com>
2022-12-03 23:27:29 +0530

    Database config

---
+++ BACKEND/server.js
@@ -0,0 +1,14 @@
+//import dependencies and assign to variables(common code)
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const app = express();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
