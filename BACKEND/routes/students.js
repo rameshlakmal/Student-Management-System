7374833ed9e0dded4a7666420a2c751921fc267f
rameshlakmal <rameshslakmal1999@gmail.com>
2022-12-04 23:18:11 +0530

    Update Student Route

--- BACKEND/routes/students.js
+++ BACKEND/routes/students.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 let Student = require("../models/Student");
 
+
+//add student method
 router.route("/add").post((req,res)=>{
     const name = req.body.name;
     const age = Number(req.body.age);
@@ -19,6 +21,7 @@ router.route("/add").post((req,res)=>{
     })
 })
 
+//get student details method
 router.route("/").get((req,res)=>{
     Student.find().then((students)=>{
         res.json(students)
@@ -26,5 +29,26 @@ router.route("/").get((req,res)=>{
         console.log(err)
     })
 })
+
+
+//Update  student details(async await)
+router.route("/update/:id").put(async (req,res)=>{
+    let userId = req.params.id; //fetch user id from url
+    const {name, age ,gender} = req.body; //destructure
+
+    const updateStudent = {
+        name,
+        age,
+        gender
+    }
+    const update = await Student.findByIdAndUpdate(userId, updateStudent).then(()=>{
+        res.status(200).send({status:"User Updated",user: update})
+    }).catch((err)=>{
+        console.log(err);
+        res.status(500).send({status:"Error with updating data"});
+    })
+})
+
+
 
 module.exports = router;
