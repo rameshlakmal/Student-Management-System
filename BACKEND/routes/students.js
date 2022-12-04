78536218647a5c65851362db42b5605b66b01e53
rameshlakmal <rameshslakmal1999@gmail.com>
2022-12-04 22:24:19 +0530

    Add Student Route

--- BACKEND/routes/students.js
+++ BACKEND/routes/students.js
@@ -1,2 +1,23 @@
 const router = require("express").Router();
 let Student = require("../models/Student");
+
+router.route("/add").post((req,res)=>{
+    const name = req.body.name;
+    const age = Number(req.body.age);
+    const gender = req.body.gender;
+
+    const newStudent = new Student({
+        name,
+        age,
+        gender
+    })
+
+    newStudent.save().then(()=>{
+        res.json("Student Added")
+    }).catch(()=>{
+        console.log(err);
+    })
+
+})
+
+module.exports = router;
