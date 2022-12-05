92ad0c930cc4c49a1947977b65aa6d4d0e89f5f3
rameshlakmal <rameshslakmal1999@gmail.com>
2022-12-05 15:58:50 +0530

    Create Student

--- frontend/src/components/AddStudent.js
+++ frontend/src/components/AddStudent.js
@@ -1,24 +1,56 @@
 import React,{useState} from "react";
+import axios from "axios";
 
 export default function AddStudent(){
 
+    const [name, setName] = useState("");
+    const [age, setAge] = useState("");
+    const [gender, setGender] = useState("");
+
+    //uda hadagattu variables walin object ekk hadagannw backend ekt e assign karagattu data tika pass karaganna
+    function sendData(e){
+        e.preventDefault();
+        const newStudent = {
+            name,
+            age,
+            gender
+        }
+        // parameters 2k use karanw 1weni ek thama back end url eka 
+        //2weni eka thama hadagattu object eka. authentication use wenaw nm parameters 3k use wenaw.
+        axios.post("http://localhost:8070/student/add",newStudent).then(()=>{
+            alert("Student Added Successful...!")
+
+        }).catch((err)=>{
+            alert(err)
+        })
+    }
+
+
     return(
     <div className="container mt-5">
-        <form className="text-left">
-            <div class="form-group">
+        <form className="text-left" onSubmit={sendData}>
+            <div className="form-group">
                 <label for="name">Student Name</label>
-                <input type="text" class="form-control" id="name" placeholder="Insert Student Name"/>
+                <input type="text" className="form-control" id="name" placeholder="Insert Student Name" 
+                //onChange eken karanne input filed wala type karana ewa realtime uda declare karapu variable walata assign karagannawa (line 6-8 , setName,setAge,setGender)
+                onChange={(e)=>{
+                    setName(e.target.value);
+                }}/>
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="age">Student Age</label>
-                <input type="number" class="form-control" id="age" placeholder="Insert Student Age"/>
+                <input type="number" className="form-control" id="age" placeholder="Insert Student Age" onChange={(e)=>{
+                    setAge(e.target.value);
+                }}/>
             </div>
-            <div class="form-group">
+            <div className="form-group">
                 <label for="gender">Student Gender</label>
-                <input type="text" class="form-control" id="gender" placeholder="Insert Student Gender"/>
+                <input type="text" className="form-control" id="gender" placeholder="Insert Student Gender"onChange={(e)=>{
+                    setGender(e.target.value);
+                }}/>
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" className="btn btn-primary" >Submit</button>
         </form>
     </div>
 
