7973ce6c1ddf6612102f779a7f9cc7e17b15cc42
rameshlakmal <rameshslakmal1999@gmail.com>
2022-12-05 18:25:25 +0530

    Fetch All Students

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom"
 import AddStudent from './components/AddStudent';
 import Header from './components/Header';
+import AllStudents from './components/AllStudents';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <Router>
       <div>
         <Header/>
+        <Route path="/" exact component={AllStudents}/>
         <Route path="/add" exact component={AddStudent} />
       </div>
     </Router>
